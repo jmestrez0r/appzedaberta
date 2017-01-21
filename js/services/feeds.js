a52c106d194c53a81fea97375021095cd1cb516f
jmestrez0r <mestre.jorge@gmail.com>
2017-01-21 18:29:39 +0000

    forcing commit

--- js/services/feeds.js
+++ js/services/feeds.js
@@ -5,10 +5,11 @@ angular.module('Elifoot').factory('Feeds', ['$http', function($http) {
           url: 'http://api.rss2json.com/v1/api.json?rss_url=http://www.zerozero.pt/rss/noticias.php',
           method: 'GET',
           dataType: 'json',
+          callback: 'JSON_CALLBACK',
           data: {
               rss_url: 'http://www.zerozero.pt/rss/noticias.php',
               api_key: '0000000000000000000000000000000000000000', // put your api key here
-              count: 2
+              count: 1
             }
         });
     }
